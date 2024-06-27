01fd11f1fe5cd3dc3d1a4fed6f58bdff6415a6bf
Sofia Arzamendia <sofia.arzamendia@hotmail.es>
2024-06-27 07:38:37 +0200

    added github link in card

--- app/components/card.jsx
+++ app/components/card.jsx
@@ -5,17 +5,21 @@ import {
 } from "react-icons/fa";
 
 
-const Card = ({title, githubLink, children}) => {
+const Card = ({ title, githubLink, children }) => {
     return (
         <div className="relative mx-auto max-w-md rounded-lg bg-gradient-to-tr from-purple-300 to-cyan-300 p-0.5 shadow-lg">
             <div className="bg-slate-800 p-7 rounded-md">
                 <div className="flex justify-between pt-2 mb-4">
                     <FaRegFolder className="text-4xl text-cyan-300 mt-[-16px]" />
-                    <FaGithub className='text-2xl text-gray-500 mt-[-10px]' />
+                    <a href={githubLink} target="_blank"
+                        rel="noopener noreferrer"
+                        className="text-gray-500 text-2xl text-gray-500 mt-[-10px] hover:text-white">
+                        <FaGithub />
+                    </a>
                 </div>
                 <h1 className="font-bold text-xl mb-2">{title}</h1>
                 {children}
-               
+
             </div>
         </div>
 
